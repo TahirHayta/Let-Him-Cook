81d9b7e98f1972c8c95373891dc08a2667b664b6
TahirHayta <147752163+TahirHayta@users.noreply.github.com>
2025-05-17 13:55:42 +0300

    pardon :D

--- Assets/Scripts/Recipe.cs
+++ Assets/Scripts/Recipe.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Recipe : ScriptableObject
 {
     public string recipeName;
-    public FoodItem[] inputItems; // the food items that are needed to create this recipe
-    public FoodItem outputItem; // the food item that will be created from the input items
+    public ItemSO[] inputItems; // the food items that are needed to create this recipe
+    public ItemSO outputItem; // the food item that will be created from the input items
     public float processTime; // the time it takes to cook/prepare this recipe    
 }
